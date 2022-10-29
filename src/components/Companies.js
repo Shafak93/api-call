f0ab8823f854e70a1b078462f75ec247bdc29547
Shafak93 <shafakctg@gmail.com>
2022-10-29 21:29:06 +0600

    fetch data functionality added to the Companies component and Loading Component design added

--- src/components/Companies.js
+++ src/components/Companies.js
@@ -1,68 +1,83 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Loading from "./Loading/Loading";
 
 const Companies = () => {
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      const url = "http://demo2211087.mockable.io/mock";
+      try {
+        const res = await fetch(url, {
+          method: "POST",
+          headers: { "content-type": "application/json" },
+        });
+        const json = await res.json();
+        const companies = json.companies;
+        setLoading(false);
+        setData(companies);
+        console.log(json.companies);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    fetchData();
+  }, [setData]);
+  if (loading == true) {
+    return <Loading />;
+  }
   return (
-    <section class="antialiased bg-gray-100 text-gray-600 h-screen px-4">
-      <div class="flex flex-col justify-center h-full">
-        <div class="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
-          <header class="px-5 py-4 border-b border-gray-100">
-            <h2 class="font-semibold text-gray-800">Companies</h2>
+    <section className="antialiased bg-gray-100 text-gray-600 h-screen px-4">
+      <div className="flex flex-col justify-center h-full">
+        <div className="w-full max-w-2xl mx-auto bg-white shadow-lg rounded-sm border border-gray-200">
+          <header className="px-5 py-4 border-b border-gray-100">
+            <h2 className="font-semibold text-gray-800">Companies</h2>
           </header>
-          <div class="p-3">
-            <div class="overflow-x-auto">
-              <table class="table-auto w-full">
-                <thead class="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
+          <div className="p-3">
+            <div className="overflow-x-auto">
+              <table className="table-auto w-full">
+                <thead className="text-xs font-semibold uppercase text-gray-400 bg-gray-50">
                   <tr>
-                    <th class="p-2 whitespace-nowrap">
-                      <div class="font-semibold text-left"></div>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left"></div>
                     </th>
-                    <th class="p-2 whitespace-nowrap">
-                      <div class="font-semibold text-left">Name</div>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Name</div>
                     </th>
-                    <th class="p-2 whitespace-nowrap">
-                      <div class="font-semibold text-left">Email</div>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Email</div>
                     </th>
-                    <th class="p-2 whitespace-nowrap">
-                      <div class="font-semibold text-left">Spent</div>
-                    </th>
-                    <th class="p-2 whitespace-nowrap">
-                      <div class="font-semibold text-center">Country</div>
+                    <th className="p-2 whitespace-nowrap">
+                      <div className="font-semibold text-left">Status</div>
                     </th>
                   </tr>
                 </thead>
-                <tbody class="text-sm divide-y divide-gray-100">
-                  <tr>
-                    <td class="p-2 whitespace-nowrap">
-                      <div class="text-left">
-                        <input type="checkbox" />
-                      </div>
-                    </td>
-                    <td class="p-2 whitespace-nowrap">
-                      <div class="flex items-center">
-                        <div class="w-10 h-10 flex-shrink-0 mr-2 sm:mr-3">
-                          <img
-                            class="rounded-full"
-                            src="https://raw.githubusercontent.com/cruip/vuejs-admin-dashboard-template/main/src/images/user-36-05.jpg"
-                            width="40"
-                            height="40"
-                            alt="Alex Shatov"
-                          />
+                <tbody className="text-sm divide-y divide-gray-100">
+                  {data.map((compani) => (
+                    <tr>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">
+                          <input type="checkbox" />
                         </div>
-                        <div class="font-medium text-gray-800">Alex Shatov</div>
-                      </div>
-                    </td>
-                    <td class="p-2 whitespace-nowrap">
-                      <div class="text-left">alexshatov@gmail.com</div>
-                    </td>
-                    <td class="p-2 whitespace-nowrap">
-                      <div class="text-left font-medium text-green-500">
-                        $2,890.66
-                      </div>
-                    </td>
-                    <td class="p-2 whitespace-nowrap">
-                      <div class="text-lg text-center">??</div>
-                    </td>
-                  </tr>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="flex items-center">
+                          <div className="font-medium text-gray-800">
+                            {compani.name}
+                          </div>
+                        </div>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left">{compani.email}</div>
+                      </td>
+                      <td className="p-2 whitespace-nowrap">
+                        <div className="text-left font-medium text-green-500">
+                          {compani.status}
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
                 </tbody>
               </table>
             </div>
---
+++ src/components/Loading/Loading.js
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Loading.css";
+
+const Loading = () => {
+  return <div class="ping"></div>;
+};
+
+export default Loading;
